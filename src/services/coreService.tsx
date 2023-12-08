9e200ae85821d65b2eb69d89f5af67d7edccbb7e
Zecheria, Adnan <adnan.zecheria@yahoo.com>
2023-12-09 00:38:21 +0200

    [wip]

--- src/services/coreService.tsx
+++ src/services/coreService.tsx
@@ -2,8 +2,55 @@ import { makepuzzle as generateNumbers } from "sudoku";
 import { GameState } from "../Components/Core/Core";
 import { getListOfIdsAssociated } from "./HelperCoreService";
 
+// Math.floor(index / 3) +
+// 1 +
+// (index / 9 >= 1
+//   ? index / 18 >= 1 // 9
+//     ? index / 36 >= 1 // 18
+//       ? index / 45 >= 1 // 9
+//         ? index / 63 >= 1 // 18
+//           ? index / 72 >= 1 // 9
+//             ? -18
+//             : -15
+//           : -12
+//         : -9
+//       : -6
+//     : -3
+//   : 0)
+
+function three(index: number) {
+  // return 0;
+  return Math.floor(index / 27) * 3;
+}
+
+function two(index: number) {
+  return Math.floor(index / 9) * 3;
+}
+
+function one(index: number) {
+  return Math.floor(index / 3);
+}
+
+function test2(index: number, row: number) {
+  // return one(index) - two(index) + three(index) + 1;
+  return (
+    Math.floor(index / 3) -
+    Math.floor(index / 9) * 3 +
+    Math.floor(index / 27) * 3 +
+    1
+  );
+}
+
 export function generateSudokuCellStates(): GameState[] {
   const unIncrementedValues: number[] = generateNumbers();
+
+  const test = unIncrementedValues.map((number, index) => {
+    let row = Math.floor(index / 9) + 1;
+
+    return test2(index, row);
+  });
+
+  console.log(test);
 
   let valueIndexZero: string;
 
